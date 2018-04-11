434afd617657886f50279b8631dc7c73f1f241a9
Thang Cao <cvthang56th2@gmail.com>
2018-04-11 21:58:11 +0700

    up system map

---
+++ js/map-script.js
@@ -0,0 +1,31 @@
+
+function initMap() {
+  var uluru = { lat: -25.363, lng: 131.044 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+
+  $('#btn1').click(function () {
+    var latln = { lat: 21.027764, lng: 105.834160 };
+    var marker1 = new google.maps.Marker({
+      position: latln,
+      map: map
+    });
+    map.setCenter(new google.maps.LatLng(21.027764, 105.834160))
+  });
+
+  
+  $('#btn2').click(function () {
+    var latln = { lat: 10.823099, lng: 106.629664 };
+    var marker1 = new google.maps.Marker({
+      position: latln,
+      map: map
+    });
+    map.setCenter(new google.maps.LatLng(10.823099, 106.629664))
+  });
+}
